065cc661e4c8e724b1dfcdfc72e8920e69aa98c5
vitaliy-pg <aljndrvt@myuax.com>
2023-11-28 09:58:31 +0100

    declaro mis dos cadenas de numeros

--- Cálculo numérico.cpp
+++ Cálculo numérico.cpp
@@ -2,7 +2,8 @@
 #include<valarray>
 using namespace std;
 int main(){
-
+    std::valarray<double> valarray1 = {1.0, 2.0, 3.0, 4.0, 5.0};
+    std::valarray<double> valarray2 = {2.0, 1.0, 2.0, 1.0, 2.0};
 
 
 

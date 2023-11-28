ed109e659ea15237d1c527707a06f88e5326bbe2
vitaliy-pg <aljndrvt@myuax.com>
2023-11-28 10:54:47 +0100

    utilizo la funcion lexical_cast

--- Introducción a Boost.cpp
+++ Introducción a Boost.cpp
@@ -2,10 +2,13 @@
 #include <boost/lexical_cast.hpp>
 
 int main(){
+    try {
+        std::string cadena = "123";
+        int numero = boost::lexical_cast<int>(cadena);
 
-
-
-
+        std::cout << "Número convertido: " << numero << std::endl;
+    } catch (const boost::bad_lexical_cast& ex) {
+        std::cerr << "Error al convertir la cadena: " << ex.what() << std::endl;
 
 
     return 0;

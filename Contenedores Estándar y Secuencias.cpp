43a32e854e8f5d3a9e96b477dbbfd9c2961cd646
vitaliy-pg <aljndrvt@myuax.com>
2023-11-28 09:19:46 +0100

    declaro un adaptador de secuencia

--- Contenedores Estándar y Secuencias.cpp
+++ Contenedores Estándar y Secuencias.cpp
@@ -4,6 +4,14 @@
 uisng namespace std;
 int main(){
     std::list<double> miLista;
+    miLista.push_back(3.14);
+    miLista.push_back(5.44);
+    miLista.push_back(2.74);
+    miLista.push_back(8.84);
+    miLista.push_back(6.31);
+    miLista.push_back(9.62);
+
+    std::stack<double, std::list<double>> miStack(miLista);
 
 
 

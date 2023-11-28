e36ff0f1f33c4e5e8e7c76dca46a1a1c36e5a512
vitaliy-pg <aljndrvt@myuax.com>
2023-11-28 10:41:58 +0100

    mejoro un pelin el codigo

--- C++ Moderno.cpp
+++ C++ Moderno.cpp
@@ -7,13 +7,13 @@ auto cuadrado = [](auto x) { return x * x; };
 
 int main(){
 
-    int numero = 5;
+    int numero = 8;
     auto resultado = cuadrado(numero);
 
     std::cout << "El cuadrado de " << numero << " es " << resultado << std::endl;
 
 // Usar expresión lambda con for_each para imprimir el cuadrado de cada elemento en un vector
-    std::vector<int> numeros = {1, 2, 3, 4, 5};
+    std::vector<int> numeros = {1, 2, 3, 4, 5, 6, 7, 8};
     std::cout << "Cuadrados de los elementos en el vector:" << std::endl;
 
     std::for_each(numeros.begin(), numeros.end(), [](auto& elemento) {

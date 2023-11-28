dfe4d28703de5638c0bd173729b4481ddaded1cc
vitaliy-pg <aljndrvt@myuax.com>
2023-11-28 08:53:46 +0100

    añado hasta el 80

--- Vector e Iteradores.cpp
+++ Vector e Iteradores.cpp
@@ -5,6 +5,14 @@ using namespace std;
 int main() {
     std::vector<int> miVector;
     miVector.push_back(10);
+    miVector.push_back(20);
+    miVector.push_back(30);
+    miVector.push_back(40);
+    miVector.push_back(50);
+    miVector.push_back(60);
+    miVector.push_back(70);
+    miVector.push_back(80);
+
 
 
 

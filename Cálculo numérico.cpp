eb81e2d9854a17e31c1d95b1831fd5cb3bfa493a
vitaliy-pg <aljndrvt@myuax.com>
2023-11-28 09:59:33 +0100

    decalaro las operaciones matematicas entre mis dos cadenas listas de nuemeros

--- Cálculo numérico.cpp
+++ Cálculo numérico.cpp
@@ -5,7 +5,10 @@ int main(){
     std::valarray<double> valarray1 = {1.0, 2.0, 3.0, 4.0, 5.0};
     std::valarray<double> valarray2 = {2.0, 1.0, 2.0, 1.0, 2.0};
 
-
+    std::valarray<double> suma = valarray1 + valarray2;
+    std::valarray<double> resta = valarray1 - valarray2;
+    std::valarray<double> multiplicacion = valarray1 * valarray2;
+    std::valarray<double> division = valarray1 / valarray2;
 
 
 

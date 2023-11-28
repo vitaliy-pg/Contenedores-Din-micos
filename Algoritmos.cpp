af1ad604a56138edcb528ec20225b5d78d12b626
vitaliy-pg <aljndrvt@myuax.com>
2023-11-28 09:43:05 +0100

    muestro el vector original

--- Algoritmos.cpp
+++ Algoritmos.cpp
@@ -8,6 +8,11 @@ int main(){
 
     std::vector<int> miVector = {7, 2, 5, 1, 8, 3, 6, 4};
 
+    std::cout << "Vector original:" << std::endl;
+    for (const auto& elemento : miVector) {
+        std::cout << elemento << " ";
+    }
+
 
 
 
